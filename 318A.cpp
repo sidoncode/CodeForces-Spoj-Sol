a4c50338d1f97ff8be30d02e9384d680a6b8ab67
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2020-01-19 13:58:46 +0530

    ds prob

---
+++ 318A.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long n, k;
+    cin >> n >> k;
+    if (k <= (n + 1) / 2){
+        cout << k * 2 - 1 << endl;
+    }
+    else{
+        cout << (k - (n + 1) / 2) * 2 << endl;
+    }
+    return 0;
+}
