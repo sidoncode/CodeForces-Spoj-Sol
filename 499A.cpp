e64bd3f8f96db02348aa273c1b19899d3dac92d2
Siddharth Sharma <siddharth54330@gmail.com>
2020-01-25 02:03:04 +0530

    partially solved for the prob of 499

---
+++ 499A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+    int T;
+    int n;
+    string s1; //fist lang
+    string s2; //2nd lang
+    cin>>T;
+    for(int i=0;i<T;i++){
+        for(int i=0;i<n;i++){
+            cin>>s1;
+        }
+        for(int i=0;i<T;i++){
+            cin>>s2;
+        }
+        for(int i=0;i<n;i++){
+            for(int i=0;i<T;i++){
+                if(s1.length() > s2.length()){
+
+                }
+                if(s1.length() < s2.length()){
+                    
+                }
+            }
+        }
+
+    }
+
+    getch();
+    return 0;
+}
