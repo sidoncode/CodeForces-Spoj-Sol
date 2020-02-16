ea813371aa0447ef214bd8ff0b22ba61b3e47132
Siddharth Sharma <siddharth54330@gmail.com>
2020-02-16 14:43:48 +0530

    commit for the 798A

---
+++ 798A.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+int main(){
+
+    int i,j,k;
+    int n;
+    string s;
+
+    cin>>s;
+    n=s.length();
+
+    if(n%2==0){
+
+        j=n-1;k=0;
+
+        for(i=0;i<n/2;i++){
+
+            if(s[i]!=s[j])
+
+                k++;
+            j--;
+        }
+
+        if(k==1)
+            cout<<"YES";
+        else
+            cout<<"NO";
+    }
+
+    else{
+        j=n-1;k=0;
+        for(i=0;i<n/2;i++,j--){
+            if(s[i]!=s[j])
+                k++;
+        }
+
+        if(k<=1)
+            cout<<"YES";
+        else
+            cout<<"NO";
+    }
+
+    return 0;
+}
