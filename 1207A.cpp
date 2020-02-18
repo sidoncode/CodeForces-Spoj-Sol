500a6cf563d8cac74bf3a5f4d149744f0cfad647
Siddharth Sharma <siddharth54330@gmail.com>
2020-02-18 14:23:27 +0530

    commit for the 1207A.cpp file

---
+++ 1207A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+
+
+        
+    int b;
+    int p;
+    int f;
+    
+    int h;
+    int c;
+
+    cin>>t;
+
+
+    while (t--){
+        
+
+
+
+    }
+    
+    
+
+
+
+    getch();
+    return 0;
+}
