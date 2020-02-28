7a16dbfb1d4db863a42bdc8ac8b96cb228e3d665
Siddharth Sharma <siddharth54330@gmail.com>
2020-02-29 00:24:25 +0530

    sol for onp question

---
+++ ONP.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+#include<conio.h>
+
+using namespace std;
+
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    string exp;
+
+    while (t--){
+
+
+
+        
+    }
+    
+    getch();
+    return 0;
+}
