1873a8a15679c69437e5a14dc5381ec84645a36f
Siddharth Sharma <siddharth54330@gmail.com>
2020-03-01 21:30:46 +0530

    sol for the problem in SPOJ

---
+++ TEST.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    int n;
+    while (true){
+        cin>>n;
+        if(n == 42){
+            break;
+        }
+        cout<<n;
+    }
+    
+    return 0;
+}
+
