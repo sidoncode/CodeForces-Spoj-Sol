0874d440a2e51f6c2a06bddb18158f4094bed21f
Siddharth Sharma <siddharth54330@gmail.com>
2020-03-31 19:13:39 +0530

    sol for the icpc

--- a.cpp
+++ a.cpp
@@ -1,38 +1,11 @@
-#include<bits/stdc++.h>
-
-
-using namespace std;
-    // for the global var; //
-
-    int v;
-    int a;
-    int r;
-    int p;
-    int h;
-
-void height();
-
-void  height(){
-    int sum = 0;
-    sum = a+r+p+v;
-    if(sum > h){
-        cout<<"WAW"<<endl;
-    }
-    else if( sum < h){
-        cout<<"AWW"<<endl;
-    }
-}
-
+#include<stdio.h>
+#include<conio.h>
 int main(){
-    
-    cin>>v>>a>>r>>p>>h;
-    
-    if( (v==a) && (a == r) && (r == p) && (p == a) ){
-    
-        cout<<"AWW"<<endl;
-    }
-    else{
-        height();
-    }
-    return 0;
+int *p;
+int a = 5;
+p = &a;
+printf("%d",&*p);
+printf("\n");
+printf("%d",*&p);
 }
+
