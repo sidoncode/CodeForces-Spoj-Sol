52c2d352373097a76b2e80c83937f2a6ee06526c
Siddharth Sharma <siddharth54330@gmail.com>
2020-04-08 18:54:04 +0530

    Add files via upload
    sol for the acmicpc comp2018

--- sola.cpp
+++ sola.cpp
@@ -1,40 +1,13 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #include<conio.h>
 
 using namespace std;
 
+
 int main(){
-    ios::sync_with_stdio(false);
-	cin.tie(0);
-    
-    int A[1000];
-    int n;
-    
-    cin>>n;
 
-    for(int i=0;i<n;i++){
-        cin>>A[i];
-    }
-    int m;
-    cin>>m;
-    int x = A[n%m];
-    //int length = sizeof(A)/sizeof(A[0]);
+    cout<<"hello"<<"\n";
 
-    if(x %2 ==0){
-        for (int i = 0; i < n;i++) {   
-            if(A[i]%2 == 0){
-                printf("%d\n", A[i]);
-            }
-        }  
-    }
-    else{
-        for (int i = 0; i < m;i++) {   
-            if(A[i]%2 != 0){
-                printf("%d\n", A[i]);
-            }
-        }  
-        
-    }getch();
-    
+    getch();
     return 0;
 }
