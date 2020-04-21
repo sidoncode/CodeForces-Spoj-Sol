de0052fb0f4d88c1d85a3ce005c31ef9d6332c26
Siddharth Sharma <siddharth54330@gmail.com>
2020-04-21 23:45:09 +0530

    commit for the sol of 1017A

---
+++ 1017A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+
+    int n;
+    cin>>n;
+    int a[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    int a;
+    int b;
+    int c;
+    int d;
+    
+
+    getch();
+    return 0;
+}
