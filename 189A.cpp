a1973d060176a89d47b64f47aba0bac0054de718
Siddharth Sharma <siddharth54330@gmail.com>
2020-04-30 01:10:21 +0530

    Add files via upload
    commit for the sol of 189A.cpp

---
+++ 189A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	int n, a, b, c, pieces = 0;
+	cin >> n >> a >> b >> c;
+
+	for (int i = 0; i*a <= n; ++i)
+	{
+		for (int j = 0; i*a + j*b <= n; ++j)
+		{
+			int k = (n - i*a - j*b)/c;
+			if (i*a + j*b + k*c == n)
+			pieces = max(i + j + k, pieces);
+		
+
+		}
+	}
+	cout << pieces <<"\n";
+
+}
