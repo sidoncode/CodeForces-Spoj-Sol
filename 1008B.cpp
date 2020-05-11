75bd9a980960ef2f95ac1b6f90c2756aafb4181f
Siddharth Sharma <siddharth54330@gmail.com>
2020-05-11 23:05:56 +0530

    Add files via upload
    commit for the sol of 1008B

---
+++ 1008B.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cin>>n;
+
+    int a[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+
+
+
+
+
+    getch();
+    return 0;
+}
