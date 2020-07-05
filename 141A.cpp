469c21da433f49b0d624aa4da52cf41e646542a5
Siddharth Sharma <siddharth54330@gmail.com>
2020-07-05 16:50:24 +0530

    Add files via upload
    commit for the sol of 141A.cpp

---
+++ 141A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+//#include<conio.h>
+
+using namespace std;
+
+int main(){ 
+
+  string a,b,c,s;
+
+    cin>>a>>b>>c;
+
+    s = a+ b;
+
+    sort(s.begin(),s.end());
+    sort(c.begin(),c.end());
+
+    if(s == c){
+        cout<<"YES"<<endl;
+
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+    
+    return 0;
+}
